50120f825e3dd1ee4f0d1f8824aa923b003ecf1f
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-08 17:44:00 +0100

    initailized lesson 7

---
+++ 07_lesson/src/features/posts/PostsList.js
@@ -0,0 +1,28 @@
+import { useSelector, } from "react-redux";
+import { selectPostIds, getPostsError, getPostsStatus} from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
+import React from 'react'
+
+const PostsList = () => {
+    const orderedPostIds = useSelector(selectPostIds);
+    const postStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError);
+    
+    let content;
+    if (postStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+    } else if (postStatus === 'succeeded') {
+        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>;
+    }
+
+    return (
+        <section>
+            {content}
+        </section>
+    )
+}
+
+export default PostsList
