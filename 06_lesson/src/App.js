a9bae0b4437361ffa9c37a051ef48fb627a7b811
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2024-04-08 17:09:45 +0100

    completed lesseon 6 RTK query

--- 06_lesson/src/App.js
+++ 06_lesson/src/App.js
@@ -1,10 +1,7 @@
+import TodoList from "./features/todos/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <TodoList />
 }
 
 export default App;
--- 06_lesson/src/index.js
+++ 06_lesson/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
-
-
